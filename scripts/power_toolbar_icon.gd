d00c7f942f1253beb51c36800200dd61a4ddc97e
Schoinas Dionysios <schinasdionisis@gmail.com>
2025-10-04 22:09:55 +0300

    Added a little text

--- scripts/power_toolbar_icon.gd
+++ scripts/power_toolbar_icon.gd
@@ -1,6 +1,12 @@
 extends MarginContainer
 
+@export var text = 1
+
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
+@onready var label: Label = %Label
+
+func _ready():
+	label.text = str(text)
 
 func _process(_delta):
 	if Input.is_action_just_pressed("button_1"):
